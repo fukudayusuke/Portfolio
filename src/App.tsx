24065591cb4cf143698b56051b0e6a46e1c0217b
fukudayusuke <fukudayusuke.fy.@gmail.com>
2023-03-05 23:10:14 +0900

    React Pro Sidebar追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+    
+  // );
+  return(
+  <>
+  <Sidebar>
+    <Menu>
+      <SubMenu label="Charts">
+        <MenuItem> Pie charts </MenuItem>
+        <MenuItem> Line charts </MenuItem>
+      </SubMenu>
+      <MenuItem> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+    </Menu>
+  </Sidebar>;
+  </>
   );
 }
 
